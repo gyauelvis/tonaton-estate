5eee62e2c1466a03475ae5f5c99fa8a1e0c32151
Gyau Boahen Elvis <66832781+gyauelvis@users.noreply.github.com>
2023-02-13 03:38:59 +0000

    Update
    You can now slide up and down the price menu. Transitions added to the hovers and alot

--- dist/index.js
+++ dist/index.js
@@ -16,6 +16,45 @@ let getFilterWindow = () => {
 };
 getRecommendedBtn.addEventListener("click", getFilterWindow);
 
-// window.addEventListener("click", (e) => {
-//   filterWindow.style.display = "none";
-// });
+//Slide up when the price is clicked
+let getpriceBtn = document.querySelector("#price");
+let slideStat = false;
+let priceFxn = (e) => {
+  e.preventDefault;
+  let getPriceMenu = document.getElementById("priceMenu");
+  let getPriceIcon = document.getElementById("price-icon");
+  if (slideStat === false) {
+    getPriceMenu.classList.add("h-0", "invisible");
+    getPriceIcon.classList.remove("-rotate-90");
+    getPriceIcon.classList.add("rotate-90");
+    slideStat = true;
+  } else if (slideStat === true) {
+    getPriceMenu.classList.remove("h-0", "invisible");
+    getPriceIcon.classList.add("-rotate-90");
+    getPriceIcon.classList.remove("rotate-90");
+    console.log(slideStat);
+    slideStat = false;
+  }
+  console.log(getPriceMenu);
+};
+getpriceBtn.addEventListener("click", priceFxn);
+
+//Showing the previous and forward btn when the various property imgs are hovered
+let getProperties = document.querySelectorAll("#properties");
+
+getProperties.forEach((prop) => {
+  prop.addEventListener("mouseover", (e) => {
+    
+  });
+});
+
+// let showIconFxn = (e) => {
+//   let getPropForwardIcons = document.querySelectorAll("#propForw");
+//   getPropForwardIcons.forEach((icon) => {
+//     icon.classList.replace("hidden", "block");
+//   });
+//   let getPropPrevIcons = document.querySelectorAll("#propPrev");
+//   getPropPrevIcons.forEach((icon) => {
+//     icon.classList.replace("hidden", "block");
+//   });
+// };
