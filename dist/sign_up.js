a435b8dbb940c7088618d82fca43419b000a13e5
Gyau Boahen Elvis <66832781+gyauelvis@users.noreply.github.com>
2023-03-09 12:07:59 +0000

    Form Validation

--- dist/sign_up.js
+++ dist/sign_up.js
@@ -2,7 +2,7 @@ const getSubmitBtn = document.querySelector("#submit");
 const getEmail = document.querySelector("#email");
 const getPassword = document.querySelector("#password");
 const getUserName = document.querySelector("#name");
-
+const passwordHelp = document.querySelector("#passwordHelp");
 getSubmitBtn.addEventListener("click", (e) => {
   e.preventDefault;
   const emailRegex = /.+@.+.\..+/;
@@ -11,11 +11,31 @@ getSubmitBtn.addEventListener("click", (e) => {
   const capsRegex = /[A-Z]/;
   const symbolRegex = /[!@#$%^&*(),.?":{}|_<>]/;
   let emailValue = getEmail.value;
-  console.log(emailValue);
-  console.log(getEmail);
+  let passwordValue = getPassword.value;
   if (emailRegex.test(emailValue)) {
-    console.log("Pk");
+    getEmail.classList.remove("border-red-700");
   } else {
-  
+    getEmail.classList.add("border-red-700");
+  }
+  if (getUserName.value == "") {
+    getUserName.classList.add("border-red-700");
+  } else {
+    getUserName.classList.remove("border-red-700");
+  }
+  if (
+    digitRegex.test(passwordValue) &&
+    smallLetterRegex.test(passwordValue) &&
+    capsRegex.test(passwordValue) &&
+    symbolRegex.test(passwordValue)
+  ) {
+    getPassword.classList.remove("border-red-700");
+    passwordHelp.textContent = "";
+  } else {
+    getPassword.classList.add("border-red-700");
+    passwordHelp.textContent =
+      "Password must contain be 8 characters long consisting of at least one symbol, an uppercase, a lower case and a number";
   }
 });
+
+const getLoginForms = document.querySelector("loginAccountForm");
+const getLoginFormsBtn = document.querySelector("")
