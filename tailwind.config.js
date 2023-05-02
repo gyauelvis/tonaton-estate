f163dc8e4182438f6d78972a170dfffe319426b3
Your Name <youremail@example.com>
2023-05-02 21:17:00 +0100

    Login and Sign-Up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
